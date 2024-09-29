d10867e1aae7e8517a6d4c0f3739316dfa474157
Gabriel <gmonteiro810@gmail.com>
2024-09-29 11:14:32 -0300

    Add project files.

---
+++ bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+//ARRAY SIMPLES
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"TAMANHO DO ARRAY {idades.Length}");
+    int acumulador = 0;
+
+    for (int i = 0; i < idades.Length; i++)
+    {
+        Console.WriteLine(idades[i]);
+        acumulador += idades[i];
+    }
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+}
+
+TestaArrayInt();
